40cebed0624d03783e2d5f096df0b1a72cfb7e21
OwenTrotto <47450694+OwenTrotto@users.noreply.github.com>
2019-05-14 07:36:25 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package org.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner keyboard = new Scanner(System.in);
+        boolean loop = true;
+        while (loop){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
