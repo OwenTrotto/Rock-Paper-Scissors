5b8048827b99e116a75ccf34edc5f4dbaae052bd
OwenTrotto <47450694+OwenTrotto@users.noreply.github.com>
2019-05-16 07:29:06 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,10 +7,31 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        int score, rock = 0, paper = 0, scissors = 0, player;
+
         Scanner keyboard = new Scanner(System.in);
         boolean loop = true;
         while (loop){
+            score = checkScore(keyboard);
+            System.out.println(" To start game over type Yes or No ");
 
+
+
+
+
+
+
+        }
+
+
+        if (rock > scissors){
+            System.out.println(" Winner gets one point ");
+        }  else if(scissors > paper){
+            System.out.println(" Winner gets one point ");
+        }  else if(paper > rock){
+            System.out.println(" Winner gets one point");
+        }  else if(rock == rock){
+            System.out.println(" No one gets a point ");
         }
 
 
@@ -18,12 +39,28 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
     }
+
+
+
+
+
+
+    public static int checkScore(Scanner keyboard){
+        int score = 0;
+        boolean wrongScore = true;
+        while (wrongScore){
+
+            System.out.println("The score is...");
+            score = keyboard.nextInt();
+            if (score > 0 && score < 4){
+                wrongScore = false;
+            }
+        }
+        return score;
+    }
+
+
+
+
 }
