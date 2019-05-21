332ddf3a3068efceaca5a30e2a814728af5961ef
Owen <owen.trotto.2021@student.lhschools.org>
2019-05-21 08:51:13 -0400

    done

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -11,43 +11,64 @@ public class Main {
 
 
 
-        int score, round, answer, rock = 0, paper = 0, scissors = 0, player = 0, player2 = 0;
+        int score, round, yes, no, answer, rock, scissors, paper, player, player2;
 
         Scanner keyboard = new Scanner(System.in);
         boolean loop = true;
         while (loop){
+
             answer = checkAnswer(keyboard);
             round = checkRound(keyboard);
-            player2 = 2;
-            System.out.println(answer);
-            System.out.println(player);
-            System.out.println(player2);
+            score = checkScore(keyboard);
 
             final String[] verb = {"rock", "paper", "scissors"};
             Random random = new Random();
             int index = random.nextInt(verb.length);
             System.out.println(verb[index]);
         }
+            player = 0;
+            player2 = 0;
+            yes = 4;
+            no = 5;
 
+
+            rock = 1;
+        paper = 2;
+        scissors = 3;
+        score = 0;
 
         if (rock > scissors){
-            System.out.println(" Rock beats Scissors, Winner gets one point");
-        }  else if(scissors > paper){
+            System.out.println(" Rock beats Scissors, player gets one point ");
+            System.out.println("score: "+ score ++);
+        }
+        if (rock > scissors){
+            System.out.println("Rock beats Scissors, player2 gets one point");
+            System.out.println("score: "+ score ++);
+        }
+        if(scissors > paper){
             System.out.println(" Scissors beats paper, Winner gets one point ");
-        }  else if(paper > rock){
+            System.out.println("score: "+ score ++);
+        }
+        if(paper > rock){
             System.out.println(" Paper beats Rock, Winner gets one point");
-        }  else if(rock == rock){
-            System.out.println(" No one gets a point ");
-        }  else if(scissors == scissors){
-            System.out.println(" No one gets a point ");
-        }  else if(paper == paper){
-            System.out.println(" No one gets a point ");
+            System.out.println("score: "+ score ++);
         }
-        if (player > player2){
-            System.out.println("Score: 1-0");
-        }   else if (player2 > player){
-            System.out.println("Score: 0-1");
+        if(rock == rock){
+            System.out.println(" No one gets a point ");
+            System.out.println("score: "+ score ++);
         }
+        if(scissors == scissors){
+            System.out.println(" No one gets a point ");
+            System.out.println("score: "+ score ++);
+        }
+        if(paper == paper){
+            System.out.println(" No one gets a point ");
+            System.out.println("score: "+ score ++);
+
+
+
+        }
+
 
 
     }
@@ -57,12 +78,13 @@ public class Main {
         boolean wrongAnswer = true;
         while (wrongAnswer){
 
-            System.out.println("To start game type Yes or No");
+            System.out.println("To start game type Yes(4) or No(5)");
             answer = keyboard.nextInt();
-            if (answer == 1 ){
+            if (answer == 4 ){
                 wrongAnswer = false;
             }
         }
+
         return answer;
     }
 
@@ -71,7 +93,7 @@ public class Main {
             boolean wrongRound = true;
             while (wrongRound){
 
-                System.out.println("Rock, Paper, Scissors, Shoot");
+                System.out.println("Player: Rock(1), Paper(2), Scissors(3), Shoot");
                 round = keyboard.nextInt();
                 if (round == 0);
                     wrongRound = false;
@@ -79,7 +101,31 @@ public class Main {
         return round;
     }
 
+    public static int checkScore(Scanner keyboard){
+        int score;
+        score = 0;
+        boolean wrongScore = true;
+        while (wrongScore){
+
+            System.out.println("Score: Player: 0 player2: "+ score++);
+            score = keyboard.nextInt();
+            if (score < 0);
+            wrongScore = false;
+        }
+        return score;
+    }
+
+    /*public static int checkWords(Scanner keyboard){
+        int words;
+        words = 0;
+        boolean wrongWords = true;
+        while (wrongWords){
+
+            System.out.println("");
+        }
+    }*/
 
 
 }
+
 
